6c59f5c55ebbfef50ce190934a406ab849f5ed10
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-04-15 04:31:05 +0700

    add navigation componen

---
+++ app/src/main/java/com/belajar/colalin/accountView/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.belajar.colalin.accountView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.belajar.colalin.R;
+
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
