0c44d1b1e357466bf31e3c6a4ad94d8db31a5244
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-04-16 20:02:23 +0700

    add bottom navigation bar and recycler view

--- app/src/main/java/com/belajar/colalin/homeView/HomeActivity.java
+++ app/src/main/java/com/belajar/colalin/homeView/HomeActivity.java
@@ -1,36 +1,59 @@
 package com.belajar.colalin.homeView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.belajar.colalin.R;
 import com.belajar.colalin.databinding.ActivityHomeBinding;
-import com.belajar.colalin.homeView.CardAdapter.Adapter;
 import com.belajar.colalin.homeView.CardAdapter.ListData;
 import com.belajar.colalin.homeView.Models.ListMenu;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    private ActivityHomeBinding binding;
-    private ArrayList< ListMenu > menus;
-
-
+    private  ArrayList<ListMenu> data ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        com.belajar.colalin.databinding.ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        BottomNavigationView bottomNavigationView = binding.bottomNavigation;
+        bottomNavigationView.setOnItemSelectedListener(navListener);
         ListData.addData();
-        menus = ListData.getData();
-        Adapter adapter = new Adapter(menus, this);
-        binding.containerViewHome.setHasFixedSize(true);
-        binding.containerViewHome.setAdapter(adapter);
-        binding.containerViewHome.setLayoutManager(new LinearLayoutManager(HomeActivity.this));
-        String data = getIntent().getStringExtra("username");
-        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+        data = ListData.getData();
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_home_container, new HomeFragment(data)).commit();
+        }
+    }
+    NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_data:
+                    fragment = new DataFragment();
+                    break;
+                case R.id.nav_profile:
+                    fragment = new ProfileFragment();
+                    break;
+                case R.id.nav_home:
+                    fragment = new HomeFragment(data);
+                    break;
+            }
+            assert fragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_home_container, fragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        data.clear();
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/belajar/colalin/homeView/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.belajar.colalin.homeView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.belajar.colalin.databinding.FragmentHomeBinding;
+import com.belajar.colalin.homeView.CardAdapter.Adapter;
+import com.belajar.colalin.homeView.Models.ListMenu;
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    private final ArrayList< ListMenu > menus;
+
+    public HomeFragment(ArrayList< ListMenu > menus) {
+        this.menus = menus;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        com.belajar.colalin.databinding.FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
+        Adapter adapter = new Adapter(menus, getContext());
+        binding.containerViewHome.setHasFixedSize(true);
+        binding.containerViewHome.setAdapter(adapter);
+        binding.containerViewHome.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
