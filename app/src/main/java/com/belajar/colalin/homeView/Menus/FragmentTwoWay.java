f5e0046103802d761c5db21108eae91542411844
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-05-07 06:50:53 +0700

    menambahkan gambar, membuat class viewmodel untuk two way

--- app/src/main/java/com/belajar/colalin/homeView/Menus/FragmentTwoWay.java
+++ app/src/main/java/com/belajar/colalin/homeView/Menus/FragmentTwoWay.java
@@ -1,22 +1,28 @@
 package com.belajar.colalin.homeView.Menus;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.belajar.colalin.R;
 import com.belajar.colalin.databinding.FragmentTwoWayBinding;
+import com.belajar.colalin.homeView.viewModel.ViewModelTwoWay;
 
 
-public class FragmentTwoWay extends Fragment {
-
+public class FragmentTwoWay extends Fragment implements View.OnClickListener {
     private FragmentTwoWayBinding binding;
+    private ViewModelTwoWay modelTwoWay;
+
     public FragmentTwoWay() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,141 @@ public class FragmentTwoWay extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentTwoWayBinding.inflate(inflater, container, false);
+        modelTwoWay = new ViewModelProvider(this).get(ViewModelTwoWay.class);
+        bindingTv();
+        bindingButton();
+        bindingTvMiror();
+        bindingButtonMiror();
         return binding.getRoot();
     }
 
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.menubar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_save:
+                    Toast.makeText(getContext(), "Save", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.nav_clear:
+                    Toast.makeText(getContext(), "Clear", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        });
+    }
+
+    public void bindingTv(){
+        binding.gol1.setText(String.valueOf(modelTwoWay.getGol_1()));
+        binding.gol2.setText(String.valueOf(modelTwoWay.getGol_2()));
+        binding.gol3.setText(String.valueOf(modelTwoWay.getGol_3()));
+        binding.gol4.setText(String.valueOf(modelTwoWay.getGol_4()));
+        binding.gol5A.setText(String.valueOf(modelTwoWay.getGol_5a()));
+        binding.gol5B.setText(String.valueOf(modelTwoWay.getGol_5b()));
+        binding.gol6A.setText(String.valueOf(modelTwoWay.getGol_6a()));
+        binding.gol6B.setText(String.valueOf(modelTwoWay.getGol_6b()));
+        binding.gol7A.setText(String.valueOf(modelTwoWay.getGol_7a()));
+        binding.gol7B.setText(String.valueOf(modelTwoWay.getGol_7b()));
+        binding.gol7C.setText(String.valueOf(modelTwoWay.getGol_7c()));
+        binding.gol8.setText(String.valueOf(modelTwoWay.getGol_8()));
+    }
+    public void bindingTvMiror(){
+        binding.mirorGol1.setText(String.valueOf(modelTwoWay.getMgol_1()));
+        binding.mirorGol2.setText(String.valueOf(modelTwoWay.getMgol_2()));
+        binding.mirorGol3.setText(String.valueOf(modelTwoWay.getMgol_3()));
+        binding.mirorGol4.setText(String.valueOf(modelTwoWay.getMgol_4()));
+        binding.mirorGol5a.setText(String.valueOf(modelTwoWay.getMgol_5a()));
+        binding.miror5b.setText(String.valueOf(modelTwoWay.getMgol_5b()));
+        binding.mirorGol6a.setText(String.valueOf(modelTwoWay.getMgol_6a()));
+        binding.mirorGol6b.setText(String.valueOf(modelTwoWay.getMgol_6b()));
+        binding.mirorGol7a.setText(String.valueOf(modelTwoWay.getMol_7a()));
+        binding.mirorGol7b.setText(String.valueOf(modelTwoWay.getMgol_7b()));
+        binding.mirorGol7c.setText(String.valueOf(modelTwoWay.getMgol_7c()));
+        binding.mirorGol8.setText(String.valueOf(modelTwoWay.getMgol_8()));
+    }
+    public void bindingButton(){
+        binding.golonganSatu.setOnClickListener(this);
+        binding.golonganDua.setOnClickListener(this);
+        binding.golonganTiga.setOnClickListener(this);
+        binding.golonganEmpat.setOnClickListener(this);
+        binding.golongan5a.setOnClickListener(this);
+        binding.golongan5b.setOnClickListener(this);
+        binding.golongan6A.setOnClickListener(this);
+        binding.golongan6B.setOnClickListener(this);
+        binding.golongan7A.setOnClickListener(this);
+        binding.golongan7B.setOnClickListener(this);
+        binding.golongan7C.setOnClickListener(this);
+        binding.golongan8.setOnClickListener(this);
+    }
+    public void bindingButtonMiror(){
+        binding.mirorBtnGol1.setOnClickListener(this);
+        binding.mirorBtnGol2.setOnClickListener(this);
+        binding.mirorBtnGol3.setOnClickListener(this);
+        binding.mirorBtnGol4.setOnClickListener(this);
+        binding.mirorBtnGol5a.setOnClickListener(this);
+        binding.mirorBtnGol5b.setOnClickListener(this);
+        binding.mirorBtnGol6a.setOnClickListener(this);
+        binding.mirorBtnGol6b.setOnClickListener(this);
+        binding.mirorBtnGol7a.setOnClickListener(this);
+        binding.mirorBtnGol7b.setOnClickListener(this);
+        binding.mirorBtnGol7c.setOnClickListener(this);
+        binding.mirorBtnGo8.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.golongan_satu) {
+            modelTwoWay.setGol_1(modelTwoWay.getGol_1() + 1);
+        }else if(view.getId() == R.id.golongan_dua){
+            modelTwoWay.setGol_2(modelTwoWay.getGol_2() + 1);
+        }else if (view.getId() == R.id.golongan_tiga){
+            modelTwoWay.setGol_3(modelTwoWay.getGol_3()+1);
+        }else if (view.getId() == R.id.golongan_empat){
+            modelTwoWay.setGol_4(modelTwoWay.getGol_4()+1);
+        }else if (view.getId() == R.id.golongan_5a){
+            modelTwoWay.setGol_5a(modelTwoWay.getGol_5a()+1);
+        }else if (view.getId() == R.id.golongan_5b){
+            modelTwoWay.setGol_5b(modelTwoWay.getGol_5b()+1);
+        }else if (view.getId() == R.id.golongan_6A){
+            modelTwoWay.setGol_6a(modelTwoWay.getGol_6a()+1);
+        }else if (view.getId() == R.id.golongan_6B){
+            modelTwoWay.setGol_6b(modelTwoWay.getGol_6b()+1);
+        }else if (view.getId() == R.id.golongan_7A){
+            modelTwoWay.setGol_7a(modelTwoWay.getGol_7a()+1);
+        }else if (view.getId() == R.id.golongan_7B){
+            modelTwoWay.setGol_7b(modelTwoWay.getGol_7b()+1);
+        }else if (view.getId() == R.id.golongan_7C){
+            modelTwoWay.setGol_7c(modelTwoWay.getGol_7c()+1);
+        }else if (view.getId() == R.id.golongan_8){
+            modelTwoWay.setGol_8(modelTwoWay.getGol_8()+1);
+        }else if (view.getId() == R.id.miror_btn_gol1){
+            modelTwoWay.setMgol_1(modelTwoWay.getMgol_1()+1);
+        }else if (view.getId() == R.id.miror_btn_gol2){
+            modelTwoWay.setMgol_2(modelTwoWay.getGol_2()+1);
+        }else if (view.getId() == R.id.miror_btn_gol3){
+            modelTwoWay.setMgol_3(modelTwoWay.getMgol_3()+1);
+        }else if (view.getId() == R.id.miror_btn_gol4){
+            modelTwoWay.setMgol_4(modelTwoWay.getMgol_4()+1);
+        }else if (view.getId() == R.id.miror_btn_gol5a){
+            modelTwoWay.setMgol_5a(modelTwoWay.getMgol_5a()+1);
+        }else if (view.getId() == R.id.miror_btn_gol5b){
+            modelTwoWay.setMgol_5b(modelTwoWay.getMgol_5b()+1);
+        }else if (view.getId() == R.id.miror_btn_gol6a){
+            modelTwoWay.setMgol_6a(modelTwoWay.getMgol_6a()+1);
+        }else if (view.getId() == R.id.miror_btn_gol6b){
+            modelTwoWay.setMgol_6b(modelTwoWay.getGol_6b()+1);
+        }else if (view.getId() == R.id.miror_btn_gol7a){
+            modelTwoWay.setMol_7a(modelTwoWay.getMol_7a()+1);
+        }else if (view.getId() == R.id.miror_btn_gol7b){
+            modelTwoWay.setMgol_7b(modelTwoWay.getMgol_7b()+1);
+        }else if (view.getId() == R.id.miror_btn_gol7c){
+            modelTwoWay.setMgol_7c(modelTwoWay.getMgol_7c()+1);
+        }else if (view.getId() == R.id.miror_btn_go8){
+            modelTwoWay.setMgol_8(modelTwoWay.getMgol_8()+1);
+        }
+        bindingTv();
+        bindingTvMiror();
+    }
 }
---
+++ app/src/main/java/com/belajar/colalin/homeView/viewModel/ViewModelTwoWay.java
@@ -0,0 +1,242 @@
+package com.belajar.colalin.homeView.viewModel;
+
+import androidx.lifecycle.ViewModel;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ViewModelTwoWay extends ViewModel {
+    private int gol_1, gol_2, gol_3, gol_4, gol_5a, gol_5b,
+            gol_6a, gol_6b, gol_7a, gol_7b, gol_7c, gol_8;
+
+    private int mgol_1, mgol_2, mgol_3, mgol_4, mgol_5a, mgol_5b,
+            mgol_6a, mgol_6b, mol_7a, mgol_7b, mgol_7c, mgol_8;
+
+    private String start;
+    private String end;
+    private String date;
+
+    public ViewModelTwoWay (){
+        this.start = DateTimeFormatter.ofPattern("HH:mm").format(LocalDateTime.now());
+        this.date = DateTimeFormatter.ofPattern("dd-MM-yyyy").format(LocalDate.now());
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getGol_1() {
+        return gol_1;
+    }
+
+    public void setGol_1(int gol_1) {
+        this.gol_1 = gol_1;
+    }
+
+    public int getGol_2() {
+        return gol_2;
+    }
+
+    public void setGol_2(int gol_2) {
+        this.gol_2 = gol_2;
+    }
+
+    public int getGol_3() {
+        return gol_3;
+    }
+
+    public void setGol_3(int gol_3) {
+        this.gol_3 = gol_3;
+    }
+
+    public int getGol_4() {
+        return gol_4;
+    }
+
+    public void setGol_4(int gol_4) {
+        this.gol_4 = gol_4;
+    }
+
+    public int getGol_5a() {
+        return gol_5a;
+    }
+
+    public void setGol_5a(int gol_5a) {
+        this.gol_5a = gol_5a;
+    }
+
+    public int getGol_5b() {
+        return gol_5b;
+    }
+
+    public void setGol_5b(int gol_5b) {
+        this.gol_5b = gol_5b;
+    }
+
+    public int getGol_6a() {
+        return gol_6a;
+    }
+
+    public void setGol_6a(int gol_6a) {
+        this.gol_6a = gol_6a;
+    }
+
+    public int getGol_6b() {
+        return gol_6b;
+    }
+
+    public void setGol_6b(int gol_6b) {
+        this.gol_6b = gol_6b;
+    }
+
+    public int getGol_7a() {
+        return gol_7a;
+    }
+
+    public void setGol_7a(int gol_7a) {
+        this.gol_7a = gol_7a;
+    }
+
+    public int getGol_7b() {
+        return gol_7b;
+    }
+
+    public void setGol_7b(int gol_7b) {
+        this.gol_7b = gol_7b;
+    }
+
+    public int getGol_7c() {
+        return gol_7c;
+    }
+
+    public void setGol_7c(int gol_7c) {
+        this.gol_7c = gol_7c;
+    }
+
+    public int getGol_8() {
+        return gol_8;
+    }
+
+    public void setGol_8(int gol_8) {
+        this.gol_8 = gol_8;
+    }
+
+    public int getMgol_1() {
+        return mgol_1;
+    }
+
+    public void setMgol_1(int mgol_1) {
+        this.mgol_1 = mgol_1;
+    }
+
+    public int getMgol_2() {
+        return mgol_2;
+    }
+
+    public void setMgol_2(int mgol_2) {
+        this.mgol_2 = mgol_2;
+    }
+
+    public int getMgol_3() {
+        return mgol_3;
+    }
+
+    public void setMgol_3(int mgol_3) {
+        this.mgol_3 = mgol_3;
+    }
+
+    public int getMgol_4() {
+        return mgol_4;
+    }
+
+    public void setMgol_4(int mgol_4) {
+        this.mgol_4 = mgol_4;
+    }
+
+    public int getMgol_5a() {
+        return mgol_5a;
+    }
+
+    public void setMgol_5a(int mgol_5a) {
+        this.mgol_5a = mgol_5a;
+    }
+
+    public int getMgol_5b() {
+        return mgol_5b;
+    }
+
+    public void setMgol_5b(int mgol_5b) {
+        this.mgol_5b = mgol_5b;
+    }
+
+    public int getMgol_6a() {
+        return mgol_6a;
+    }
+
+    public void setMgol_6a(int mgol_6a) {
+        this.mgol_6a = mgol_6a;
+    }
+
+    public int getMgol_6b() {
+        return mgol_6b;
+    }
+
+    public void setMgol_6b(int mgol_6b) {
+        this.mgol_6b = mgol_6b;
+    }
+
+    public int getMol_7a() {
+        return mol_7a;
+    }
+
+    public void setMol_7a(int mol_7a) {
+        this.mol_7a = mol_7a;
+    }
+
+    public int getMgol_7b() {
+        return mgol_7b;
+    }
+
+    public void setMgol_7b(int mgol_7b) {
+        this.mgol_7b = mgol_7b;
+    }
+
+    public int getMgol_7c() {
+        return mgol_7c;
+    }
+
+    public void setMgol_7c(int mgol_7c) {
+        this.mgol_7c = mgol_7c;
+    }
+
+    public int getMgol_8() {
+        return mgol_8;
+    }
+
+    public void setMgol_8(int mgol_8) {
+        this.mgol_8 = mgol_8;
+    }
+
+
+}
