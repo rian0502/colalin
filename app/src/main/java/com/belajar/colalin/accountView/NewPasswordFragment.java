f71837a7dc121bcceabf3cdb0a8e360e9c48f3ce
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-05-10 19:28:35 +0700

    ubah data update gradle

--- app/src/main/java/com/belajar/colalin/accountView/NewPasswordFragment.java
+++ app/src/main/java/com/belajar/colalin/accountView/NewPasswordFragment.java
@@ -1,16 +1,13 @@
 package com.belajar.colalin.accountView;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.belajar.colalin.accountView.viewModelAcc.ViewModelNewPass;
 import com.belajar.colalin.databinding.FragmentNewPasswordBinding;
 
--- app/src/main/java/com/belajar/colalin/apiService/Kendaraan.java
+++ app/src/main/java/com/belajar/colalin/apiService/Kendaraan.java
@@ -40,6 +40,42 @@ public class Kendaraan {
     @SerializedName("gol_8")
     @Expose
     private String gol8;
+    @SerializedName("mir_gol1")
+    @Expose
+    private String mirGol1;
+    @SerializedName("mir_gol2")
+    @Expose
+    private String mirGol2;
+    @SerializedName("mir_gol3")
+    @Expose
+    private String mirGol3;
+    @SerializedName("mir_gol4")
+    @Expose
+    private String mirGol4;
+    @SerializedName("mir_gol5a")
+    @Expose
+    private String mirGol5a;
+    @SerializedName("mir_gol5b")
+    @Expose
+    private String mirGol5b;
+    @SerializedName("mir_gol6a")
+    @Expose
+    private String mirGol6a;
+    @SerializedName("mir_gol6b")
+    @Expose
+    private String mirGol6b;
+    @SerializedName("mir_gol7a")
+    @Expose
+    private String mirGol7a;
+    @SerializedName("mir_gol7b")
+    @Expose
+    private String mirGol7b;
+    @SerializedName("mir_gol7c")
+    @Expose
+    private String mirGol7c;
+    @SerializedName("mir_gol8")
+    @Expose
+    private String mirGol8;
 
     public String getGol1() {
         return gol1;
@@ -135,5 +171,101 @@ public class Kendaraan {
 
     public void setGol8(String gol8) {
         this.gol8 = gol8;
+    }
+
+    public String getMirGol1() {
+        return mirGol1;
+    }
+
+    public void setMirGol1(String mirGol1) {
+        this.mirGol1 = mirGol1;
+    }
+
+    public String getMirGol2() {
+        return mirGol2;
+    }
+
+    public void setMirGol2(String mirGol2) {
+        this.mirGol2 = mirGol2;
+    }
+
+    public String getMirGol3() {
+        return mirGol3;
+    }
+
+    public void setMirGol3(String mirGol3) {
+        this.mirGol3 = mirGol3;
+    }
+
+    public String getMirGol4() {
+        return mirGol4;
+    }
+
+    public void setMirGol4(String mirGol4) {
+        this.mirGol4 = mirGol4;
+    }
+
+    public String getMirGol5a() {
+        return mirGol5a;
+    }
+
+    public void setMirGol5a(String mirGol5a) {
+        this.mirGol5a = mirGol5a;
+    }
+
+    public String getMirGol5b() {
+        return mirGol5b;
+    }
+
+    public void setMirGol5b(String mirGol5b) {
+        this.mirGol5b = mirGol5b;
+    }
+
+    public String getMirGol6a() {
+        return mirGol6a;
+    }
+
+    public void setMirGol6a(String mirGol6a) {
+        this.mirGol6a = mirGol6a;
+    }
+
+    public String getMirGol6b() {
+        return mirGol6b;
+    }
+
+    public void setMirGol6b(String mirGol6b) {
+        this.mirGol6b = mirGol6b;
+    }
+
+    public String getMirGol7a() {
+        return mirGol7a;
+    }
+
+    public void setMirGol7a(String mirGol7a) {
+        this.mirGol7a = mirGol7a;
+    }
+
+    public String getMirGol7b() {
+        return mirGol7b;
+    }
+
+    public void setMirGol7b(String mirGol7b) {
+        this.mirGol7b = mirGol7b;
+    }
+
+    public String getMirGol7c() {
+        return mirGol7c;
+    }
+
+    public void setMirGol7c(String mirGol7c) {
+        this.mirGol7c = mirGol7c;
+    }
+
+    public String getMirGol8() {
+        return mirGol8;
+    }
+
+    public void setMirGol8(String mirGol8) {
+        this.mirGol8 = mirGol8;
     }
 }
