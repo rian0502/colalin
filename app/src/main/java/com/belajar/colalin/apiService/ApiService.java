9994ed89c54aa99ce6b86d742f0b87c10a61cccb
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-05-11 22:44:04 +0700

    add service

--- app/src/main/java/com/belajar/colalin/apiService/ApiService.java
+++ app/src/main/java/com/belajar/colalin/apiService/ApiService.java
@@ -39,9 +39,6 @@ public interface ApiService {
     @POST("show_counter.php")
     Call<ArrayList< ListCounter >> getList(@Field("id_akun") int id);
 
-    @FormUrlEncoded
-    @POST("show_detail.php")
-    Call<ArrayList<ListCounter>> getDetaill();
 
     @FormUrlEncoded
     @POST("register.php")
@@ -49,5 +46,78 @@ public interface ApiService {
                                                       @Field("username") String username,
                                                       @Field("phone") String phone
     );
+    @FormUrlEncoded
+    @POST("insert_one.php")
+    Call<ArrayList<StatusRespons>> insert_one(@Field("lokasi")String lokasi,
+                                              @Field("jenis") String jenis,
+                                              @Field("date") String date,
+                                              @Field("start") String start,
+                                              @Field("end") String end,
+                                              @Field("id_akun") int id,
+                                              @Field("gol1") int gol1,
+                                              @Field("gol2") int gol2,
+                                              @Field("gol3") int gol3,
+                                              @Field("gol4") int gol4,
+                                              @Field("gol5a") int gol5a,
+                                              @Field("gol5b") int gol5b,
+                                              @Field("gol6a") int gol6a,
+                                              @Field("gol6b") int gol6b,
+                                              @Field("gol7a") int gol7a,
+                                              @Field("gol7b") int gol7b,
+                                              @Field("gol7c") int gol7c,
+                                              @Field("gol8") int gol8
+    );
+    @FormUrlEncoded
+    @POST("insert_toll.php")
+    Call<ArrayList<StatusRespons>> insert_toll(@Field("lokasi")String lokasi,
+                                              @Field("jenis") String jenis,
+                                              @Field("date") String date,
+                                              @Field("start") String start,
+                                              @Field("end") String end,
+                                              @Field("id_akun") int id,
+                                              @Field("gol2") int gol2,
+                                              @Field("gol3") int gol3,
+                                              @Field("gol4") int gol4,
+                                              @Field("gol5a") int gol5a,
+                                              @Field("gol5b") int gol5b,
+                                              @Field("gol6a") int gol6a,
+                                              @Field("gol6b") int gol6b,
+                                              @Field("gol7a") int gol7a,
+                                              @Field("gol7b") int gol7b,
+                                              @Field("gol7c") int gol7c
 
+    );
+    @FormUrlEncoded
+    @POST("insert_two.php")
+    Call<ArrayList<StatusRespons>> insert_two(@Field("lokasi")String lokasi,
+                                              @Field("jenis") String jenis,
+                                              @Field("date") String date,
+                                              @Field("start") String start,
+                                              @Field("end") String end,
+                                              @Field("id_akun") int id,
+                                              @Field("gol1") int gol1,
+                                              @Field("gol2") int gol2,
+                                              @Field("gol3") int gol3,
+                                              @Field("gol4") int gol4,
+                                              @Field("gol5a") int gol5a,
+                                              @Field("gol5b") int gol5b,
+                                              @Field("gol6a") int gol6a,
+                                              @Field("gol6b") int gol6b,
+                                              @Field("gol7a") int gol7a,
+                                              @Field("gol7b") int gol7b,
+                                              @Field("gol7c") int gol7c,
+                                              @Field("gol8") int gol8,
+                                              @Field("mgol1") int mgol1,
+                                              @Field("mgol2") int mgol2,
+                                              @Field("mgol3") int mgol3,
+                                              @Field("mgol4") int mgol4,
+                                              @Field("mgol5a") int mgol5a,
+                                              @Field("mgol5b") int mgol5b,
+                                              @Field("mgol6a") int mgol6a,
+                                              @Field("mgol6b") int mgol6b,
+                                              @Field("mgol7a") int mgol7a,
+                                              @Field("mgol7b") int mgol7b,
+                                              @Field("mgol7c") int mgol7c,
+                                              @Field("mgol8") int mgol8
+    );
 }
