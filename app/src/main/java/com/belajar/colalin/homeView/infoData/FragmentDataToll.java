806ad9cb30b63ec3ce7c408ceced6d21a2c08d93
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-05-22 06:20:53 +0700

    selesai

--- app/src/main/java/com/belajar/colalin/homeView/infoData/FragmentDataToll.java
+++ app/src/main/java/com/belajar/colalin/homeView/infoData/FragmentDataToll.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.belajar.colalin.databinding.FragmentDataTollBinding;
 import com.belajar.colalin.homeView.viewModel.ViewModelDataToll;
 
